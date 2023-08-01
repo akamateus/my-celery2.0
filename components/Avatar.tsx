249b4f8a8a8b10b30af26217d009146612e29604
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-01 11:58:36 +0200

    added avatar

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,3 +1,8 @@
+import useUser from "@/hooks/useUser";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useCallback } from "react";
+
 interface AvatarProps {
   userId: string;
   isLarge?: boolean;
@@ -5,7 +10,44 @@ interface AvatarProps {
 }
 
 const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
-  return <div></div>;
+  const router = useRouter();
+  const { data: fetchedUser } = useUser(userId);
+  const onClick = useCallback(
+    (event: any) => {
+      event.stopPropagation();
+
+      const url = `/users/${userId}`;
+
+      router.push(url);
+    },
+    [router, userId]
+  );
+
+  return (
+    <div
+      className={`
+    ${hasBorder ? "border-4 border black" : ""}
+    ${isLarge ? "h-32" : "h-12"}
+    ${isLarge ? "w32" : "w-12"}
+    rounded-full
+    hover:opacity-90
+    transition
+    cursor-pointer
+    relative
+    `}
+    >
+      <Image
+        fill
+        style={{
+          objectFit: "cover",
+          borderRadius: "100%",
+        }}
+        alt="Avatar"
+        onClick={onClick}
+        src={fetchedUser?.profileImage || `/images/celeryAvatar.png`}
+      />
+    </div>
+  );
 };
 
 export default Avatar;
