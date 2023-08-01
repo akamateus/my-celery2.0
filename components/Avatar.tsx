ccf1a6641193140c689e08f38e30d25b1710e0a2
Mateus Oliveira <mateusoliveirac@hotmail.com>
2023-08-01 14:03:44 +0200

    added avatar on the profile

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -26,12 +26,12 @@ const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
   return (
     <div
       className={`
-    ${hasBorder ? "border-4 border black" : ""}
+    ${hasBorder ? "border-4 border-stone-900" : ""}
     ${isLarge ? "h-32" : "h-12"}
-    ${isLarge ? "w32" : "w-12"}
-    rounded-full
-    hover:opacity-90
-    transition
+    ${isLarge ? "w-32" : "w-12"}
+    rounded-full 
+    hover:opacity-90 
+    transition 
     cursor-pointer
     relative
     `}
--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -4,6 +4,7 @@ import { BeatLoader } from "react-spinners";
 import useUser from "@/hooks/useUser";
 
 import Header from "@/components/Header";
+import UserHero from "@/components/users/UserHero";
 
 const UserView = () => {
   const router = useRouter();
@@ -22,6 +23,7 @@ const UserView = () => {
   return (
     <>
       <Header showBackArrow label={fetchedUser?.name} />
+      <UserHero userId={userId as string} />
     </>
   );
 };
